e384cad9eb4f502849d45c2a2dc9048249d222f0
vova <Vladimir Budilov>
2016-11-20 01:30:38 -0500

    Changed the pool id

--- src/providers/properties.service.ts
+++ src/providers/properties.service.ts
@@ -5,9 +5,8 @@
 export let _REGION = "us-east-1";
 
 export let _IDENTITY_POOL_ID = "us-east-1:35e3d90d-4ae2-4c77-871d-4671551eaaa0";
-export let _USER_POOL_ID = "us-east-1_r9R2BPLNs";
-export let _CLIENT_ID = "4pap8sr2bucimte357h4i5d4gf";
-export let _G_MAPS_API_KEY = "AIzaSyA4h9CcA45WWJHF9z0ssIlPqiCO3FToQLU";
+export let _USER_POOL_ID = "us-east-1_bybDCnVM9";
+export let _CLIENT_ID = "3hl0eo4321kv7i3i2amqa8k8jf";
 
 export let _POOL_DATA = {
   UserPoolId: _USER_POOL_ID,
