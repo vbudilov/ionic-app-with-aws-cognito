af41f83fb5456ca094af138396f8e62813309507
vova <Vladimir Budilov>
2016-11-20 21:30:11 -0500

    Updated identity pool id

--- src/providers/properties.service.ts
+++ src/providers/properties.service.ts
@@ -4,7 +4,7 @@
 
 export let _REGION = "us-east-1";
 
-export let _IDENTITY_POOL_ID = "us-east-1:35e3d90d-4ae2-4c77-871d-4671551eaaa0";
+export let _IDENTITY_POOL_ID = "us-east-1:688daf8f-a234-423e-9222-4d489c796834";
 export let _USER_POOL_ID = "us-east-1_bybDCnVM9";
 export let _CLIENT_ID = "3hl0eo4321kv7i3i2amqa8k8jf";
 export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
