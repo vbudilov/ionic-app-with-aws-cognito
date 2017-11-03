0f8526ba0f37e656754ec1ddfc060890d61acdc7
Matt Masters <Matthew.Masters.MM@gmail.com>
2017-11-03 19:56:38 +0000

    Update Cognito settings

--- src/providers/properties.service.ts
+++ src/providers/properties.service.ts
@@ -1,12 +1,8 @@
-/**
- * Created by Vladimir Budilov on 8/13/16.
- */
+export let _REGION = "eu-central-1";
 
-export let _REGION = "us-east-1";
-
-export let _IDENTITY_POOL_ID = "us-east-1:688daf8f-a234-423e-9222-4d489c796834";
-export let _USER_POOL_ID = "us-east-1_bybDCnVM9";
-export let _CLIENT_ID = "3hl0eo4321kv7i3i2amqa8k8jf";
+export let _IDENTITY_POOL_ID = "us-east-1:washer";
+export let _USER_POOL_ID = "eu-central-1_hnYZAZirL";
+export let _CLIENT_ID = "43qnar46qni71j77fvknjjmfli";
 export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
 
 export let _POOL_DATA = {
